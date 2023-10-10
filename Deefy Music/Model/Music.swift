de3cc8a18d313bd898cceaefe8eb0167d458cc27
AyakorK <guillaume.moret@yahoo.com>
2023-10-10 21:08:33 +0200

    Modify class of musics

--- Deefy Music/Model/Music.swift
+++ Deefy Music/Model/Music.swift
@@ -7,18 +7,16 @@ import Foundation
 struct Music {
     // Music composed of -> Title, Artists, Album, Image, Duration
     var title: String
-    var artists: [String]
-    var album: String
-    var image: String
+    var artists: Any
+    var album: Album
     var duration: Int
 
 
     // Init
-    init(title: String, artists: [String], album: String, image: String, duration: Int) {
+    init(title: String, artists: Any, album: Album, duration: Int) {
         self.title = title
         self.artists = artists
         self.album = album
-        self.image = image
         self.duration = duration
     }
 }
