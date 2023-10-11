f06d26e4da38329cbbca65387b954959e7516b3a
Ilan Petiot <91156471+IlanHopti@users.noreply.github.com>
2023-10-11 09:21:57 +0200

    Display images with label

---
+++ Deefy Music/Controller/HomeViewController.swift
@@ -0,0 +1,67 @@
+//
+//  HomeViewController.swift
+//  Deefy Music
+//
+//  Created by Ilan Petiot on 10/10/2023.
+//
+
+import UIKit
+
+extension UIImage {
+    func resize(to size: CGSize) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: size)
+        return renderer.image { (context) in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
+    }
+}
+
+class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+
+    @IBOutlet weak var HomeCollectionView: UICollectionView!
+    var images : [Images] = [Images(image: "Haikyuuu", name: "Haikyuuu"), Images(image: "Experiences", name: "Experiences"), Images(image: "Darius", name: "Darius"), Images(image: "Garen", name: "Garen"), Images(image: "android", name: "android"),Images(image: "Haikyuuu", name: "Haikyuuu"), Images(image: "Experiences", name: "Experiences"), Images(image: "Darius", name: "Darius"), Images(image: "Garen", name: "Garen"), Images(image: "android", name: "android")]
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)as! HomeCollectionViewCell
+//        cell.ImagesCell.image = UIImage(named: images[indexPath.row].image)
+        if let image = UIImage(named: images[indexPath.row].image) {
+
+            let targetSize = CGSize(width: 175, height: 150)
+
+            let resizedImage = image.resize(to: targetSize)
+
+            cell.ImagesCell.image = resizedImage
+            cell.ImagesCell.contentMode = .scaleAspectFit
+            cell.labelImage.text = images[indexPath.row].name
+        }
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Deefy Music/Controller/ViewController.swift
+++ Deefy Music/Controller/ViewController.swift
@@ -15,12 +15,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
 
         if (UserDefaults.standard.bool(forKey: "isLogged")) {
-            if let collectionViewController = storyboard.instantiateViewController(withIdentifier: "homeView") as? HomeCollectionViewController {
-                let flowLayout = UICollectionViewFlowLayout()
-                collectionViewController.collectionView.collectionViewLayout = flowLayout
-                self.navigationController?.pushViewController(collectionViewController, animated: true)
-            }
+//            if let collectionViewController = storyboard.instantiateViewController(withIdentifier: "homeView") as? HomeCollectionViewController {
+//                let flowLayout = UICollectionViewFlowLayout()
+//                collectionViewController.collectionView.collectionViewLayout = flowLayout
+//                self.navigationController?.pushViewController(collectionViewController, animated: true)
+//            }
 //           self.navigationController?.pushViewController(HomeCollectionViewController(), animated: true)
+
+//            if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Home") as? HomeViewController {
+////                self.present(vc, animated:true, completion: nil)
+//                self.navigationController?.pushViewController(vc, animated: true)
+//            }
+
+             let collectionViewController = storyboard.instantiateViewController(withIdentifier: "tabBar")
+                self.navigationController?.pushViewController(collectionViewController, animated: true)
+
         }
     }
 
