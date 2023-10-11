e565e4a358efc447ba9439662a66df60489db792
Ilan Petiot <91156471+IlanHopti@users.noreply.github.com>
2023-10-11 12:15:47 +0200

    Add working searchBar

--- Deefy Music/Controller/SearchViewController.swift
+++ Deefy Music/Controller/SearchViewController.swift
@@ -7,34 +7,34 @@
 
 import UIKit
 
-class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating {
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var SearchTableView: UITableView!
 
     var search: [Search] = [Search(image: "Haikyuuu", artist: "Haruichi", title: "Haikyuuu"), Search(image: "Darius", artist: "Riot", title: "Darius"), Search(image: "Haikyuuu", artist: "Haruichi", title: "Haikyuuu"), Search(image: "Haikyuuu", artist: "Haruichi", title: "Haikyuuu"), Search(image: "Haikyuuu", artist: "Haruichi", title: "Haikyuuu"), Search(image: "Haikyuuu", artist: "Haruichi", title: "Haikyuuu"), Search(image: "Haikyuuu", artist: "Haruichi", title: "Haikyuuu")]
-
-    let searchController = UISearchController()
+    var filteredData: [Search]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Search"
-        searchController.searchResultsUpdater = self
-        navigationItem.searchController = searchController
 
+        filteredData = search
         SearchTableView.dataSource = self
         SearchTableView.delegate = self
+        searchBar.delegate = self
         // Do any additional setup after loading the view.
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return search.count
+        return filteredData.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell") as! SearchTableViewCell
-        cell.Title.text = search[indexPath.row].title
-        cell.Artist.text = search[indexPath.row].artist
-        cell.MusicImage.image = UIImage(named: search[indexPath.row].image)
+        cell.Title.text = filteredData[indexPath.row].title
+        cell.Artist.text = filteredData[indexPath.row].artist
+        cell.MusicImage.image = UIImage(named: filteredData[indexPath.row].image)
         return cell
     }
 
@@ -48,13 +48,20 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
 //        }
 //      }
 
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let text = searchController.searchBar.text else { return }
-        print(text)
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+        filteredData = []
+        if searchText == "" {
+            filteredData = search
+        } else {
+            for search in search {
+                if search.title.lowercased().contains(searchText.lowercased()) {
+                    filteredData.append(search)
+                }
+            }
+        }
+        self.SearchTableView.reloadData()
     }
-
-
-
 
 /*
     // MARK: - Navigation
