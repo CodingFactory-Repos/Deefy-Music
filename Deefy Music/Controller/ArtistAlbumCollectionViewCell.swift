e8b87595970c97eb73a3da1b2ed7300077b80d32
Lucas Goncalves <87122664+RuKaSuu@users.noreply.github.com>
2023-10-12 19:58:33 +0200

    Creation of the storyboard and of the controller for the artists view

---
+++ Deefy Music/Controller/ArtistAlbumCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArtistAlbumCollectionViewCell.swift
+//  Deefy Music
+//
+//  Created by Loucassoulet on 12/10/2023.
+//
+
+import UIKit
+
+class ArtistAlbumCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var albumCover: UIView!
+    
+    @IBOutlet weak var albumTitle: UILabel!
+}
